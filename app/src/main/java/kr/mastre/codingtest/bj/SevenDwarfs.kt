440807c32aab7464e204d6c87ccdf0036a2beea3
Mastre <peaceyop@gmail.com>
2023-07-21 09:22:09 +0900

    백준 문제 풀이 일곱난쟁이 -> 간단했는데 2중 loop 안에서 break 했는데 inner loop만 종료되는 버그로 시간 걸림. 2중 loop에서 break 사용할 때 주의하자.

---
+++ app/src/main/java/kr/mastre/codingtest/bj/SevenDwarfs.kt
@@ -0,0 +1,26 @@
+package kr.mastre.codingtest.bj
+
+fun main() {
+    val totalHeight = 100
+    val dwarfs = 9
+    val heights = IntArray(dwarfs)
+
+    repeat(dwarfs) {
+        heights[it] = readln().toInt()
+    }
+
+    heights.sort()
+    val sumHeights = heights.sum()
+
+    all@ for (i in 0 until dwarfs - 1) {
+        for (j in i + 1 until dwarfs) {
+            val minus = heights[i] + heights[j]
+            if (sumHeights - minus == totalHeight) {
+                heights[i] = -1
+                heights[j] = -1
+                heights.filter { it != -1 }.forEach { println(it) }
+                break@all
+            }
+        }
+    }
+}
